fa7e432a124e65702e7d26cbba6ba60345c6d2ef
UllasDas004 <ullasdas110904@gmail.com>
2026-03-31 21:24:29 +0530

    Stats updated

--- scripts/update_stats.py
+++ scripts/update_stats.py
@@ -6,10 +6,10 @@ ASSETS_DIR = os.path.join(BASE_DIR, "assets")
 
 # --- 📊 UPDATE YOUR STATS HERE 📊 ---
 STATS = {
-    "stat_pokedex.png": ("Caught", "700+", "LeetCode Questions"),
-    "stat_kanto.png": ("Kanto BP", "1860", "LeetCode Rating"),
-    "stat_sinnoh.png": ("Sinnoh BP", "1486", "Codeforces Rating"),
-    "stat_hoenn.png": ("Hoenn BP", "1633", "CodeChef Rating")
+    "stat_pokedex.png": ("Caught", "750+", "LeetCode Questions"),
+    "stat_kanto.png": ("Kanto BP", "1921", "Knight at LeetCode"),
+    "stat_sinnoh.png": ("Sinnoh BP", "1519", "Specialist at Codeforces"),
+    "stat_hoenn.png": ("Hoenn BP", "1650", "3 Star at CodeChef")
 }
 
 HEADER_TEXT = "LEAGUE STATS"
